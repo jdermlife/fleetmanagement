b956182de955e1c3a0b5ec9d9a98951ab3f8b3b5
Jorge <jdioneda@gmail.com>
2026-06-08 13:15:59 +0800

    commit and push

--- backend/main.py
+++ backend/main.py
@@ -8,14 +8,6 @@ from app.routes.ai import router as ai_router
 from app.routes.lease import router as lease_router
 from app.routes.database import router as database_router
 
-app.include_router(lease_router)
-app.include_router(database_router)
-
-
-app.include_router(driver_router)
-app.include_router(ai_router)
-
-
 
 
 app = FastAPI()
@@ -35,6 +27,12 @@ app.add_middleware(
 
 app.include_router(driver_router)
 app.include_router(ai_router)
+
+app.include_router(lease_router)
+app.include_router(database_router)
+
+
+
 
 @app.get("/")
 def home():
