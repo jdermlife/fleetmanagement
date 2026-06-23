b2aabdd9942c7d899d33372ee2db5683fe0c4eae
Jorge <jdioneda@gmail.com>
2026-06-23 19:14:57 +0800

    Be Ahead

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -170,7 +170,7 @@ const govMenuItems = menuLinks.filter(
             </h2>
 
             <p className="app-brand-subtitle">
-              No Limits to Getting Ahead
+              Be Ahead
             </p>
           </div>
 
