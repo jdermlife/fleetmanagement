d2f880b78747b80320abd1c7113487e34df76320
Jorge <jdioneda@gmail.com>
2026-07-14 10:22:08 +0800

    financial GPS

--- frontend/src/brand.ts
+++ frontend/src/brand.ts
@@ -1,5 +1,5 @@
 import brandLogoImage from './assets/filscore-logo-refined.png'
 
 export const APP_NAME = 'FILSCORE'
-export const APP_TAGLINE = 'Borrow Smart. Lend Right.'
+export const APP_TAGLINE = 'Your financial GPS'
 export const brandLogoDataUri = brandLogoImage
